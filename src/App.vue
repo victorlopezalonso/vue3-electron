e64661f059a01ec894e011bc1c6eb00e6aa5f92b
Víctor L <vlopez@quadram.mobi>
2021-11-19 13:24:04 +0100

    feat: Add tailwindcss configuration

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <HelloWorld msg="Vue 3 + Electron App + TailwindsCSS" />
 </template>
 
 <script>
@@ -19,8 +18,6 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
