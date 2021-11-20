e1a16af5edc8aecfaf673fe89bba54dbcf698ece
Víctor L <vlopez@quadram.mobi>
2021-11-20 11:46:58 +0100

    feat: Add IPC manager

--- src/background.js
+++ src/background.js
@@ -4,6 +4,7 @@ import { app, protocol, BrowserWindow } from 'electron'
 import { createProtocol } from 'vue-cli-plugin-electron-builder/lib'
 import installExtension, { VUEJS3_DEVTOOLS } from 'electron-devtools-installer'
 const isDevelopment = process.env.NODE_ENV !== 'production'
+import "../src/lib/ipcHandler.js";
 
 // Scheme must be registered before the app is ready
 protocol.registerSchemesAsPrivileged([
@@ -20,7 +21,9 @@ async function createWindow() {
       // Use pluginOptions.nodeIntegration, leave this alone
       // See nklayman.github.io/vue-cli-plugin-electron-builder/guide/security.html#node-integration for more info
       nodeIntegration: process.env.ELECTRON_NODE_INTEGRATION,
-      contextIsolation: !process.env.ELECTRON_NODE_INTEGRATION
+      contextIsolation: !process.env.ELECTRON_NODE_INTEGRATION,
+      //Allow CORS
+      // webSecurity: false,
     }
   })
 
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -6,23 +6,16 @@
         <img class="mx-auto" alt="Vue logo" src="@/assets/logo.png" />
         <h1 class="mb-5 text-5xl font-bold">{{ title }}</h1>
         <p class="mb-5 text-2xl">{{ msg }}</p>
-        <a href="/components/modal#my-modal" class="btn btn-primary">
-          <BellIcon class="h-5 w-5 inline-block" /> open modal
-        </a>
-      </div>
-    </div>
-  </div>
-  <div id="my-modal" class="modal">
-    <div class="modal-box">
-      <p>Hey There!</p>
-      <div class="modal-action">
-        <a href="/components/modal#" class="btn btn-primary">Accept</a>
+        <span class="btn btn-primary" @click="showDialog">
+          <BellIcon class="h-5 w-5 inline-block" /> open ICP dialog
+        </span>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import icp from "../lib/ipc";
 import { BellIcon } from "@heroicons/vue/solid";
 export default {
   props: {
@@ -30,5 +23,10 @@ export default {
     msg: String,
   },
   components: { BellIcon },
+  methods: {
+    showDialog() {
+      icp.showDialog("This is an electron dialog");
+    },
+  },
 };
 </script>
